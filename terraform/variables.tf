f1400687c5c3397401104485a6c390667e83fb4e
silvemerson <araujo.emerson28@gmail.com>
2025-03-06 21:41:47 -0300

    ajustado tipos de instancias

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,7 +18,7 @@ variable "default_zone" {
 
 
 variable "machine_type" {
-  default     = "e2-standard-2"
+  default     = "e2-standard-4"
   type        = string
-  description = "Tamanho dos nodes do cluster gke"
+  description = "Tamanho das EC2"
 }
